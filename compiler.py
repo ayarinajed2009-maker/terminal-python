dc4a369e4fd3e1d3cad8db6fdeba9fd8f44f351b
ayarinajed2009-maker <ayarinajed2009@gmail.com>
2026-09-18 16:58:17 +0000

    Adding a new file: functions/terminal.py that contains a start function which will be used to start the terminal. The function currently has a placeholder for the command variable and a help command that prints out available commands. and making compaler ready for reading new type of code is code.cpx and add terminal run if doesnt found it !

--- compiler.py
+++ compiler.py
@@ -2,6 +2,16 @@
 from functions.system import *
 from functions.video import *
 from functions.get import *
-get.update_list(True)
-get.uninstall("winrar")
-get.install("winrar")
+from functions.terminal import *
+if exist("code.npx"):
+    with open('code.npx', 'r', encoding='utf-8') as file:
+        file = file.read()
+elif not exist("code.npx"):
+    print("no code.npx file to run found !")
+    print("opening terminal instead...")
+    run("cls")
+    print("Terminal v0.1 beta")
+    print("Type 'help' to see all commands")
+    while True:
+        command = input(">>> ")
+        start(command)
---
+++ functions/terminal.py
@@ -0,0 +1,116 @@
+from functions.system import *
+from functions.video import *
+from functions.get import *
+def start():
+    if command == "help":
+            print("Commands:")
+            print("help - shows this help message")
+            print("exit - exits the terminal")
+            print("update - updates the terminal")
+            print("info <app> - shows information about an app")
+    elif command == "exit":
+            break
+    elif command == "update":
+            get.self_update()
+    elif command.startswith("info "):
+            app = command.split(" ")[1]
+            get.info(app)
+    elif command.startswith("install "):
+            app = command.split(" ")[1]
+            get.install(app)
+    elif command.startswith("update_list"):
+            get.update_list(True)
+    elif command.startswith("update "):
+            app = command.split(" ")[1]
+            get.update(app)
+    elif command.startswith("uninstall "):
+            app = command.split(" ")[1]
+            get.uninstall(app)
+    elif command.startswith("read "):
+            file = command.split(" ")[1]
+            read(file,True)
+    elif command.startswith("mod "):
+            file = command.split(" ")[1]
+            txt = command.split(" ")[2]
+            mod(file,txt,True)
+    elif command.startswith("creat_file "):
+            path = command.split(" ")[1]
+            name = command.split(" ")[2]
+            creat_file(path,name,True)
+    elif command.startswith("delete_file "):
+            path = command.split(" ")[1]
+            name = command.split(" ")[2]
+            delete_file(path,name,True)
+    elif command.startswith("cd "):
+            path = command.split(" ")[1]
+            cd(path,True)
+    elif command.startswith("mkdir "):
+            path = command.split(" ")[1]
+            name = command.split(" ")[2]
+            mkdir(path,name,True)
+    elif command.startswith("notify "):
+            title = command.split(" ")[1]
+            message = command.split(" ")[2]
+            notify(title,message)
+    elif command.startswith("play "):
+            path = command.split(" ")[1]
+            play(path)
+    elif command.startswith("vlc "):
+            file = command.split(" ")[1]
+            play_vlc(file)
+    elif command.startswith("rmdir "):
+            path = command.split(" ")[1]
+            name = command.split(" ")[2]
+            rmdir(path,name,True)
+    elif command.startswith("help "):
+            command = command.split(" ")[1]
+            if command == "install":
+                print("install <app> - installs an app using winget")
+            elif command == "update_list":
+                print("update_list - shows a list of apps that can be updated using winget")
+            elif command == "update":
+                print("update <app> - updates an app using winget")
+            elif command == "uninstall":
+                print("uninstall <app> - uninstalls an app using winget")
+            elif command == "info":
+                print("info <app> - shows information about an app using winget")
+            elif command == "read":
+                print("read <file> - reads the contents of a file")
+            elif command == "mod":
+                print("mod <file> <text> - modifies the contents of a file with the specified text")
+            elif command == "creat_file":
+                print("creat_file <path> <name> - creates a new file at the specified path with the specified name")
+            elif command == "delete_file":
+                print("delete_file <path> <name> - deletes a file at the specified path with the specified name")
+            elif command == "cd":
+                print("cd <path> - changes the current directory to the specified path")
+            elif command == "mkdir":
+                print("mkdir <path> <name> - creates a new directory at the specified path with the specified name")
+            elif command == "rmdir":
+                print("rmdir <path> <name> - removes a directory at the specified path with the specified name")
+            elif command == "notify":
+                print("notify <title> <message> - sends a notification with the specified title and message")
+            elif command == "play":
+                print("play <path> - plays a media file at the specified path using the default media player")
+            elif command == "vlc":
+                print("vlc <file> - plays a media file using VLC media player")
+            else:
+                print("install <app> - installs an app using winget")
+                print("update_list - shows a list of apps that can be updated using winget")
+                print("update <app> - updates an app using winget")
+                print("uninstall <app> - uninstalls an app using winget")
+                print("info <app> - shows information about an app using winget")
+                print("read <file> - reads the contents of a file")
+                print("mod <file> <text> - modifies the contents of a file with the specified text")
+                print("creat_file <path> <name> - creates a new file at the specified path with the specified name")
+                print("delete_file <path> <name> - deletes a file at the specified path with the specified name")
+                print("cd <path> - changes the current directory to the specified path")
+                print("mkdir <path> <name> - creates a new directory at the specified path with the specified name")
+                print("rmdir <path> <name> - removes a directory at the specified path with the specified name")
+                print("notify <title> <message> - sends a notification with the specified title and message")
+                print("play <path> - plays a media file at the specified path using the default media player")
+                print("vlc <file> - plays a media file using VLC media player")
+    else:
+            print("Unknown command: "+command)
+            print("Try update the terminal to get the latest commands !")
+            print("Type 'help' to see all commands")
