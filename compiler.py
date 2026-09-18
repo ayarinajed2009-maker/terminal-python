33f6da1efcaea738386f93f5afd34a6806bdadff
ayarinajed2009-maker <ayarinajed2009@gmail.com>
2026-09-18 16:05:35 +0000

    add get to install/uninstall/update functions

--- compiler.py
+++ compiler.py
@@ -1,7 +1,4 @@
 #compiler v0.1 beta
-#You can find Help page to know how to use the terminal
-#run with run.bat
 from functions.system import *
 from functions.video import *
-for i in range(10):
-    notify(str(generate_chaine(10)), str(generate_code(10)))
+from functions.get import *
---
+++ functions/get.py
@@ -0,0 +1,32 @@
+from functions.system import *
+class get():
+    def install(app):
+        run("winget install "+app)
+    def update(app):
+        run("winget update "+app)
+    def update_list(pas):
+        run("winget upgrade >temp.txt")
+        if pas:
+            test=True
+        elif not pas:
+            test=exist("temp.txt")
+        if test:
+                run("type temp.txt")
+        else:
+            print("ERROR 305 : fail to get informations !")
+    def uninstall(app):
+        run("winget uninstall "+app)
+    def self_update():
+        run("mkdir temps && cd temps && wget https://raw.githubusercontent.com/ayarinajed2009-maker/terminal-python/refs/heads/main/version -O version")
+        with open('functions/version', 'r', encoding='utf-8') as file:
+            local_update = file.read()
+        with open('temps/version', 'r', encoding='utf-8') as file:
+            server_update = file.read()
+        if local_update != server_update:
+            run("rmdir /s /q temps && wget https://raw.githubusercontent.com/ayarinajed2009-maker/terminal-python/refs/heads/main/functions/needs.py -O functions/needs.py && wget https://raw.githubusercontent.com/ayarinajed2009-maker/terminal-python/refs/heads/main/functions/system.py -O functions/system.py && wget https://raw.githubusercontent.com/ayarinajed2009-maker/terminal-python/refs/heads/main/functions/video.py -O functions/video.py && wget https://raw.githubusercontent.com/ayarinajed2009-maker/terminal-python/refs/heads/main/version -O functions/version")
+        else:
+            run("rmdir /s /q temps")
+            print("No update available !")
+    def info(app):
+        run("winget show "+app)
+            
